1b19a08b060dac9196d9fe47fd8e4e27490de085
Serigzhan Naviy <serigzhan_naviy@epam.com>
2025-12-19 13:30:19 +0500

    implement custom serialization logic

---
+++ Custom Serialization/Person.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+
+namespace Custom.Serialization;
+
+[Serializable]
+public class Person : ISerializable
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public Person() { }
+
+    public Person(int id, string name) { Id = id; Name = name; }
+
+    protected Person(SerializationInfo info, StreamingContext context)
+    {
+
+        Id = info.GetInt32("custom_id");
+        Name = info.GetString("custom_name");
+
+    }
+
+    public void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+
+        info.AddValue("custom_id", Id);
+        info.AddValue("custom_name", Name);
+
+    }
+}
---
+++ Custom Serialization/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Custom.Serialization;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        Person person = new Person(1, "Mike");
+
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        using (FileStream fs = new FileStream("person.bin", FileMode.Create))
+        {
+
+            formatter.Serialize(fs, person);
+
+        }
+
+        using (FileStream fs = new FileStream("person.bin", FileMode.Open))
+        {
+
+            Person newPerson = (Person)formatter.Deserialize(fs);
+
+            Console.WriteLine("New person info: {0}: {1}", newPerson.Id, newPerson.Name);
+
+        }
+
+        Console.Read();
+
+    }
+}
